41bbe4ad112b66253608ed8a6c8df042ddb121a0
leandroaa7 <leandroaalexandre@gmail.com>
2020-03-20 11:47:19 -0300

    implementando add questao e update questao utilizando a classe List

--- src/routes/questionario.js
+++ src/routes/questionario.js
@@ -3,7 +3,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-   res.render('pesquisador/questionario')
+   res.render('pesquisador/questionario', { title: "questionario" })
 });
+
+router.post('/', (req,res)=> {
+   res.json(req.body);
+})
 
 module.exports = router;
--- src/views-controller/pesquisador/questionario.js
+++ src/views-controller/pesquisador/questionario.js
@@ -1,16 +1,59 @@
 import M from 'materialize-css';
 import Container from '../Container';
 import AddEvent from '../AddEvent';
+import Form from '../Form';
+
+class List {
+    constructor(ulId) {
+        this.ulId = ulId;
+        this.ulObject = document.getElementById(ulId);
+        this.liList = this.ulObject.children;
+    }
+
+    //retorna array de objeto de todos os elementos li e atributos desejados, como "data-peso" por exemplo
+    getData() {
+
+    }
+}
 
 class QuestionarioController {
-    constructor(){
-        let questaoInputsId = ["id","titulo","peso"];
-        this.container = new Container("questao",questaoInputsId);
+    constructor() {
+        let questaoInputsId = ["id", "titulo", "peso"];
+        this.container = new Container("questionario", questaoInputsId);
+    }
+
+    //eventos que devem ser adicionados na DOM, verificar na classe AddEvents
+    getEvents() {
+        return {
+            newEvent: true,
+            deleteEvent: true,
+            deleteCancelEvent: true
+        }
+    }
+
+    //nome do controller
+    getName = () => {
+        return "questionario";
+    }
+
+    goToAdd = () => {
+        let action = "/questionario/";
+        let method = "POST";
+        this.container.goToAdd(action, method);
+    }
+
+    goToUpdate = () => {
+        let action = "/questionario/atualizar";
+        let method = "POST";
+
     }
 }
 
 
 document.addEventListener('DOMContentLoaded', () => {
+    new AddEvent(new QuestionarioController());
+
+    Form.configForm();
     var elems = document.querySelectorAll('.collapsible');
     M.Collapsible.init(elems);
 });
